91783bf612fcf2d84bef7364a907e8238a9a93d2
Yrutan <simon.roy.26@hotmail.com>
2015-12-18 11:15:53 -0500

    better start for AI
    lumière rebuilded

---
+++ Source/MultimediaFinal/Default_AI_Controller.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "Default_AI_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIMEDIAFINAL_API ADefault_AI_Controller : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
