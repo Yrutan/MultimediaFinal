9a8cbc8238621df77efc533f1d166a24bd9dd26f
Yrutan <simon.roy.26@hotmail.com>
2015-12-22 00:50:55 -0500

    multiplayer vraiment plus compliqué que prévu
    texte pour moi
    A simple model for something like a health pack pickup could look like
    this:
    On client, player presses use, call ServerPickupObject(TheObject).
    On server, ServerPickupObject verifies that it's ok, updates the
    player's health, and calls ClientPickupObject.
    
    On client, ClientPickupObject does the cosmetic picking up (animation,
    audio, effects, etc). Could also do this immediately on client without
    waiting for server verification, which would reduce latency but could
    result in seeing pickup cosmetics when it really failed.
    Player's updated health replicates to the client via normal variable
    replication.

--- Source/MultimediaFinal/MultimediaFinal.h
+++ Source/MultimediaFinal/MultimediaFinal.h
@@ -4,3 +4,6 @@
 
 #include "Engine.h"
 
+// Ce n'est pas inclus par défaut! C'est nécessaire pour la réplication et l'initialisation de
+// GetLifetimeReplicatedProps
+#include "Net/UnrealNetwork.h"
